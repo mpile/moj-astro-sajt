f1795695613305dceceeb4ad9ee711ee10e99076
mpile <milos.m.piletic@gmail.com>
2026-02-25 14:23:33 +0100

    POboljšanja u slanju kontakt forme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,5 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-import partytown from '@astrojs/partytown';
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [
-    partytown({
-      config: {
-        forward: ["dataLayer.push"],
-      },
-    }),
-  ],
+  // Nema partytown integracije
 });
